11ef047a5bfa15d15f6b2c8218b74e62c1ecfc14
egou550 <e.go.ullate@gmail.com>
2024-11-21 17:25:20 +0100

    Primer commit

---
+++ CleanCodeNBproject/src/unavarra/is/practica7/ejercicios/DNI.java
@@ -0,0 +1,13 @@
+
+package unavarra.is.practica7.ejercicios;
+
+/**
+ *
+ * @author alumno
+ */
+public class DNI {
+    private String[] letras = {"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+    public String devolverLetra (int n){
+        return letras[n];
+    }
+}
---
+++ CleanCodeNBproject/src/unavarra/is/practica7/ejercicios/Paciente.java
@@ -0,0 +1,41 @@
+package unavarra.is.practica7.ejercicios;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author alumno
+ */
+public class Paciente {
+    private String nombre;
+    private float peso;
+    private float altura;
+    private int anioNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Paciente(String nombre, float peso, float altura, int anioNacimiento) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.altura = altura;
+        this.anioNacimiento = anioNacimiento;
+    }
+    
+    private int calcularEdad(){ 
+        return LocalDate.now().getYear() - anioNacimiento;
+    }
+    
+    private float calcularImc(){
+        return (float) (peso / Math.pow(altura, 2));
+    }
+    
+    public boolean problema(){
+        if(calcularImc() >= 25){
+           if(calcularEdad() > 65){
+               return true;
+           }    
+        }  
+        return false;
+    }
+}
